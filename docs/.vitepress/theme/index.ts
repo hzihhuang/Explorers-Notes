039507305b37497fdab6674a24c1a2294f0732e9
hzihhuang <3306084512@qq.com>
2024-10-24 01:32:27 +0000

    perf: 渐变色

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -6,3 +6,15 @@ export default {
   ...DefaultTheme,
   Layout
 };
+
+// 检查浏览器，添加到类以进行条件样式
+if (typeof window !== 'undefined') {
+  const browser = navigator.userAgent.toLowerCase()
+  if (browser.includes('chrome')) {
+    document.documentElement.classList.add('browser-chrome')
+  } else if (browser.includes('firefox')) {
+    document.documentElement.classList.add('browser-firefox')
+  } else if (browser.includes('safari')) {
+    document.documentElement.classList.add('browser-safari')
+  }
+}
