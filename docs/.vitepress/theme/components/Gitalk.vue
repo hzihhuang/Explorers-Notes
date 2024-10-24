1bc26b4681a99c0c7ffde9fb28ef6e68c1301dfc
hzihhuang <61496900+hzihhuang@users.noreply.github.com>
2024-10-24 04:07:17 +0000

    perf: 优化

--- docs/.vitepress/theme/components/Gitalk.vue
+++ docs/.vitepress/theme/components/Gitalk.vue
@@ -1,23 +1,27 @@
 .
 <script setup lang="ts">
-import Gitalk from 'gitalk'
-import 'gitalk/dist/gitalk.css';
-import usePageId from '../utils/usePageId';
-import { computed } from 'vue';
+import Gitalk from "gitalk";
+import "gitalk/dist/gitalk.css";
+import usePageId from "../utils/usePageId";
+import { computed, onMounted } from "vue";
 
 const pageId = usePageId();
 
 const options = computed(() => ({
-    clientID: 'Ov23li8KldjgRlDCK5Zs',
-    clientSecret: 'f1dd1dcdaadbaeddb5630c449f9fd476d35c27fa',
-    repo: 'Explorers-Notes-Gitalk',
-    owner: 'hzihhuang',
-    admin: ['hzihhuang', 'hzihhuang-site'],
-    id: pageId,      // 页面的唯一标识
-    distractionFreeMode: false  // 弹幕模式
-}))
+  clientID: "Ov23li8KldjgRlDCK5Zs",
+  clientSecret: "f1dd1dcdaadbaeddb5630c449f9fd476d35c27fa",
+  repo: "Explorers-Notes-Gitalk",
+  owner: "hzihhuang",
+  admin: ["hzihhuang", "hzihhuang-site"],
+  id: pageId, // 页面的唯一标识
+  distractionFreeMode: false, // 弹幕模式
+}));
+
+onMounted(() => {
+  new Gitalk(options).render("gitalk-container");
+});
 </script>
 
 <template>
-    <Gitalk :options="options" />
+  <div id="gitalk-container" />
 </template>
